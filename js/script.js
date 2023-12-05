ab35633bc926755c3a203fe6ac72c2605126033c
GiordanoDiego <diegomgiordano96@gmail.com>
2023-12-05 15:22:04 +0100

    completed no-bonus

--- js/script.js
+++ js/script.js
@@ -12,3 +12,27 @@ document.getElementById('idHtml').innerHTML = noveVariabileJS;
 
 */
 
+for(let i = 1; i <= 100; i++){
+
+    if(i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz", i, i % 3, i % 5);
+        document.getElementById('lista').innerHTML += "FizzBuzz" + " ";
+    }
+    else if (i % 3 == 0 ){
+        console.log("Fizz", i, i % 3, i % 5);
+        document.getElementById('lista').innerHTML += "Fizz" + " ";
+
+    }
+    else if(i % 5 == 0){
+        console.log("Buzz", i, i % 3, i % 5);
+        document.getElementById('lista').innerHTML += "Buzz" + " ";
+
+    }
+    else{
+        console.log(i, i % 3, i % 5)//test OK
+        document.getElementById('lista').innerHTML += i + " ";
+
+    } 
+}
+
+// document.getElementById('lista').innerHTML += i + 1 + "\n";
